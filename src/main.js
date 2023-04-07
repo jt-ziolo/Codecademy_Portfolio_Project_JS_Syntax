c7363bd2941969ddad04929ccdfd9f2bcfb5579c
JT Ziolo <ziolojt@gmail.com>
2023-04-06 18:24:17 -0600

    feat(phrases): add identifications and phrases arrays

--- src/main.js
+++ src/main.js
@@ -3,12 +3,26 @@
 // 1. An introduction, (e.g. "May I have your attention?")
 // 2. Identifying the bride and groom in some way (e.g. "To the new Mr. and Mrs.")
 // 3. Two encouraging and wishful phrases combined, (e.g. "May they live together happily forever.")
+// The phrases must be different
 
 const introductions = [
     'Excuse me! May I have your attention?',
     'Ahem. Check, check. We\'re good? Great. Hey everyone, may I have a few words?',
     'Hey everyone! Hello? Ah, thank you. I\'d like to make a toast.',
     'May I have your attention please? Thank you. Let us raise our glasses tonight to celebrate...',
+];
+
+const identifications = [
+    'To the bride and groom',
+    'To the new Mr. and Mrs.',
+    'To the newly wedded couple',
+    'To the newlyweds',
+];
+
+const phrases = [
+    'May thy life be long and happy',
+    'May they remain lovers for all of life',
+    'May your wedding days be few and your anniversaries many',
 ];
 
 const randomFromArray = (arr) => {
